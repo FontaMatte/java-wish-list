d9798663b3a08c7bcc902ef6ea518552f32deddc
FontaMatte <matteo.fontanel@gmail.com>
2023-06-09 16:16:50 +0200

    Bonus Completed

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,9 +1,30 @@
 package org.lessons.java.christmas;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    private final static String FILE_PATH = "./resources/gifts.txt";
+
+    public static void main(String[] args)  {
+
+        Scanner reader = null;
+        try {
+            reader = new Scanner(new File(FILE_PATH));
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
 
         Scanner scanner = new Scanner(System.in);
 
@@ -31,6 +52,36 @@ public class Main {
         System.out.println("Total list length: " + gifts.size());
         for (String gift: gifts ){
             System.out.println("- " + gift);
+        }
+
+        scanner.close();
+
+        File file = new File(FILE_PATH);
+        boolean fileExists = file.exists();
+
+        // Open FileWriter
+        FileWriter writer = null;
+        try {
+            if (!fileExists) {
+                writer = new FileWriter(FILE_PATH);
+                writer.write("GIFTS LIST \n");
+            } else {
+                writer = new FileWriter(FILE_PATH, true);
+            }
+
+            for (String gift: gifts ){
+                writer.write("- " + gift.toString() + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println("unable to write file");
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
     }
